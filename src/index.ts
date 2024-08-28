74e916465b27a039314760a0f3f08df9d8059458
GBFerro <giovaniferro29@gmail.com>
2024-08-27 23:17:06 -0300

    add variavel de ambiente

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("/api/users/register", (req, res) => {
+app.options("/api/users/register", (_req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
